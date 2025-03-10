931ef9538262782312b26fb7b4a35c46343dda7c
youseokhwan <youseokhwan15@gmail.com>
2025-03-10 17:51:04 +0900

    feat: BaseballGame 구현

---
+++ seokhwan/BaseballGame/BaseballGame/BaseballGame.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+struct BaseballGame {
+    private enum Message: String {
+        case startGame = "< 게임을 시작합니다 >"
+        case pleaseInputNumber = "숫자를 입력하세요\n>> "
+        case invalidInput = "올바르지 않은 입력값입니다"
+        case correctAnswer = "정답입니다!\n"
+        case wrongAnswer = "오답입니다!\n"
+    }
+
+    func start() {
+        let correctAnswer = randomAnswer()
+        print(.startGame)
+
+        while true {
+            print(.pleaseInputNumber, terminator: "")
+
+            guard let input = readLine(),
+                  let answer = Int(input) else {
+                print(.invalidInput)
+                continue
+            }
+
+            if answer == correctAnswer {
+                print(.correctAnswer)
+                break
+            } else {
+                print(.wrongAnswer)
+            }
+        }
+    }
+
+    private func print(_ message: BaseballGame.Message, terminator: String = "\n") {
+        Swift.print(message.rawValue, terminator: terminator)
+    }
+
+    private func randomAnswer() -> Int {
+        return Int.random(in: 0..<9)
+    }
+}
--- seokhwan/BaseballGame/BaseballGame/main.swift
+++ seokhwan/BaseballGame/BaseballGame/main.swift
@@ -1,3 +1,4 @@
 import Foundation
 
-print("Hello, World!")
+let game = BaseballGame()
+game.start()
