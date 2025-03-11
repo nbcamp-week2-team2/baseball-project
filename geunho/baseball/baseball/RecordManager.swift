e9909d95a1166d6adc878bc99d734e225fe9d177
EllGHT <rmsgh1188@gmail.com>
2025-03-11 16:47:14 +0900

    feat: 게임 기록 보기 추가

---
+++ geunho/baseball/baseball/RecordManager.swift
@@ -0,0 +1,29 @@
+//
+//  RecordManager.swift
+//  baseball
+//
+//  Created by 정근호 on 3/11/25.
+//
+
+import Foundation
+
+class RecordManager {
+    
+    var recordsList = [String]()
+    var records = [Int:Int]()
+    
+    func showRecords() {
+        print("\n< 게임 기록 보기 >")
+        for record in records {
+            recordsList.append(("\(record.key)번째 게임 : 시도 횟수 - \(record.value)"))
+        }
+        recordsList.sort()
+        
+        recordsList.forEach {
+            print($0)
+        }
+        
+    }
+    
+    
+}
