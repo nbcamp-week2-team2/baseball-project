2ed9e4cf9bd638ac0cb4c36a24724e501d713c67
EllGHT <rmsgh1188@gmail.com>
2025-03-11 16:47:02 +0900

    fix: main 구조 변경

--- geunho/baseball/baseball/main.swift
+++ geunho/baseball/baseball/main.swift
@@ -11,42 +11,34 @@ enum GameState: String {
     case start = "1. 게임 시작하기"
     case log = "2. 게임 기록 보기"
     case quit = "3. 종료하기"
-    
-    func continueGame() {
-        switch self {
-        case .start:
-            game.start()
-        case .log:
-            game.log()
-        case .quit:
-            game.quit()
-        }
-    }
 }
+
 
 let game = BaseballGame()
-let gameState: GameState
-var selectState: String
 
-print("환영합니다! 원하시는 번호를 입력해주세요!")
-print("\(GameState.start.rawValue)  \(GameState.log.rawValue)  \(GameState.quit.rawValue)")
+welcome()
 
-selectState = readLine() ?? ""
-
-switch selectState {
-case "1":
-    gameState = .start
-    gameState.continueGame()
-case "2":
-    gameState = .log
-    gameState.continueGame()
-case "3":
-    gameState = .quit
-    gameState.continueGame()
-default:
-    print("올바른 번호를 입력해주세요")
+func welcome() {
+    var gameState: GameState
+    var selectedState: String
+    
+    print("\n환영합니다! 원하시는 번호를 입력해주세요!")
+    print("\(GameState.start.rawValue)  \(GameState.log.rawValue)  \(GameState.quit.rawValue)")
+    
+    selectedState = readLine() ?? ""
+    
+    switch selectedState {
+    case "1":
+        gameState = .start
+        game.start()
+    case "2":
+        gameState = .log
+        game.log()
+    case "3":
+        gameState = .quit
+        game.quit()
+    default:
+        print("올바른 번호를 입력해주세요")
+    }
 }
-
-
-
 
