dd5f552a99bb020c0bca0d7fb97e02348090d2b5
Seungho Jang <sjang1594@gmail.com>
2025-03-11 20:28:34 +0900

    Complete lvl5
    1. show logs

--- shjang/shjang/GameComponent/BaseballGame.swift
+++ shjang/shjang/GameComponent/BaseballGame.swift
@@ -3,50 +3,96 @@ import Foundation
 final class BaseballGame {
     private var answer: Int = 0
     private var userInput: String = ""
-    private var currentResult: Dictionary<String, Int> = ["strike": 0, "ball": 0]
-    // private var history = Dictionary<Int, Array>()
+    private var currentTimeInterval: Date!
+    private var histories: [RecordHistory] = []
     
     init() {
-        self.answer = generateAnswer()
+        answer = generateAnswer()
     }
     
-    /* start game loop */
+    /// Logic:       Start
+    /// Description: This is the main Game Loop (depending on the user input, this will loop infinite**
     func start() {
         print("Game Start")
+        resetData()
         while (true) {
-            print("Enter the number")
+            displayMenu()
+            let ch = readLine()!
+            switch (ch) {
+            case "1":
+                playGame()
+            case "2":
+                showGameRecord()
+            case "3":
+                print("Game Exit")
+                resetData()
+                return
+            default:
+                print("Invalid ")
+            }
+        }
+    }
+    
+    /// Utils:
+    private func displayMenu() {
+        print("1. Game Start\t2.Game Record\t3.Exit")
+    }
+    
+    private func playGame() {
+        print("Enter the Number")
+        currentTimeInterval = Date()
+        print(answer)
+        var currentData = RecordHistory()
+        while (true) {
             userInput = readLine()!
-            if guess(){
+            if guess(record: &currentData) {
                 print("Game End")
+                let currentTimeEndInterval = Date()
+                currentData.totalTime = currentTimeEndInterval.timeIntervalSince(currentTimeInterval)
+                saveGameHistory(currentData)
                 break;
             }
         }
     }
     
-    /* makeAnswer (generate) would fit */
-    private func generateAnswer() -> Int {
-        var gen = SeedRandomNumberGenerator(seed: Int(Date().timeIntervalSince1970))
-        let num = Int(gen.next())
-        // Check the Input Value
-        if hasRepetitiveNumber(num) {
-            return generateAnswer()
-        }
-        return num;
-    }
-    
-    private func guess() -> Bool {
+    /* Game Logic */
+    private func guess(record: inout RecordHistory) -> Bool {
         if !validate(userInput: userInput){
             return false
         }
         
-        let (strike, ball) = calcScore()
-        currentResult["strike"] = strike
-        currentResult["ball"] = ball
-        print(currentResult["strike"]!, currentResult["ball"]!)
-        return strike == 3
+        var strikeBallRecord = StrikeBallRecord()
+        record.attempts += 1
+        calcScore(record: &strikeBallRecord)
+        record.records.append(strikeBallRecord)
+        print(strikeBallRecord.strikeCount, strikeBallRecord.ballCount)
+        return strikeBallRecord.strikeCount == 3
     }
     
-    private func calcScore() -> (strike: Int, ball: Int) {
+    private func saveGameHistory(_ history: RecordHistory) {
+        histories.append(history)
+    }
+    
+    private func resetData() {
+        histories.removeAll()
+    }
+    
+    private func showGameRecord() {
+        print("< Show Records >")
+        if histories.count <= 0 {
+            print("No Record Found")
+        } else {
+            for (index, history) in histories.enumerated() {
+                print("Game Number: \(index + 1)")
+                print("Iteration: \(history.attempts)")
+                print("Total Time: \(history.totalTime) sec")
+                print()
+            }
+        }
+    }
+    
+    /* Game Logic */
+    private func calcScore(record: inout StrikeBallRecord) {
         let answerStr = String(answer)
         var strike = 0
         var ball = 0
@@ -69,8 +115,22 @@ final class BaseballGame {
                 answerSet.remove(userInput[idx])
             }
         }
-        
-        return (strike, ball)
+        record.strikeCount = strike
+        record.ballCount = ball
+    }
+    
+    /// Utils:           generateAnswer & makeAnswer
+    /// Description:     Generate the Random Number Depending on the seed
+    /// Parameters Type: nil
+    /// Return:          Int
+    private func generateAnswer() -> Int {
+        var gen = SeedRandomNumberGenerator(seed: Int(Date().timeIntervalSince1970))
+        let num = Int(gen.next())
+        // Check the Input Value
+        if hasRepetitiveNumber(num) {
+            return generateAnswer()
+        }
+        return num;
     }
 
     // TODO: Return this as Error Code
@@ -99,11 +159,19 @@ final class BaseballGame {
         return true
     }
     
+    /// Utils:           hasRepetitiveNumber
+    /// Description:     Check whether there are repetitive number
+    /// Parameters Type: Int
+    /// Return:          Bool
     private func hasRepetitiveNumber(_ input: Int) -> Bool {
         let digits = String(input)
         return Set(digits).count < digits.count
     }
     
+    /// Utils:           isFirstDigitZero
+    /// Description:     Check whether the first digit is zero
+    /// Parameters Type: String
+    /// Return:          Bool
     private func isFirstDigitZero(_ input: String) -> Bool {
         return input.first == "0"
     }
--- shjang/shjang/Utils/ErrorEnum.swift
+++ shjang/shjang/Utils/ErrorEnum.swift
@@ -1,11 +1,14 @@
 /*  Define Error Code */
 enum ErrorCode {
+    case generalInputError
     case exceedMaxLength
     case invalidInputType
     case repetitiveNumbers
     case isFirstDigitZero
     var localizedDescription: String {
         switch self {
+        case .generalInputError:
+            return "The user input is invalid"
         case .invalidInputType:
             return "The user input is supposed to be a string"
         case .exceedMaxLength:
