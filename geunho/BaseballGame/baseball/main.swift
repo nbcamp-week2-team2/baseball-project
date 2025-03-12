9baf4fc70b2a4d829d52ab35678b632c500fd64e
EllGHT <rmsgh1188@gmail.com>
2025-03-12 11:40:59 +0900

    fix: main에서 올바르지 않은 입력 시 재실행

--- geunho/BaseballGame/baseball/main.swift
+++ geunho/BaseballGame/baseball/main.swift
@@ -18,22 +18,25 @@ let game = BaseballGame()
 welcome()
 
 func welcome() {
-    var selectedState: String
-    
-    print("\n환영합니다! 원하시는 번호를 입력해주세요!")
-    print("\(GameState.start.rawValue)  \(GameState.log.rawValue)  \(GameState.quit.rawValue)")
-    
-    selectedState = readLine() ?? ""
-    
-    switch selectedState {
-    case "1":
-        game.start()
-    case "2":
-        game.log()
-    case "3":
-        game.quit()
-    default:
-        print("올바른 번호를 입력해주세요")
+    while true {
+        var selectedState: String
+        
+        print("\n환영합니다! 원하시는 번호를 입력해주세요!")
+        print("\(GameState.start.rawValue)  \(GameState.log.rawValue)  \(GameState.quit.rawValue)")
+        
+        selectedState = readLine() ?? ""
+        
+        switch selectedState {
+        case "1":
+            game.start()
+        case "2":
+            game.log()
+        case "3":
+            game.quit()
+        default:
+            print("올바른 번호를 입력해주세요")
+            continue
+        }
     }
 }
 
